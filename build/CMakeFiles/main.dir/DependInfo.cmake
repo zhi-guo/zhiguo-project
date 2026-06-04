15d9697b3e9c663c40f725075d09e27f4d3713db
zhiguo <3632436955@qq.com>
2026-06-05 00:54:42 +0800

    首次提交：上传项目初始代码

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/code/hello_world/main.c" "C:/code/hello_world/build/CMakeFiles/main.dir/main.c.obj"
+  "C:/code/hello_world/print.c" "C:/code/hello_world/build/CMakeFiles/main.dir/print.c.obj"
+  "C:/code/hello_world/print1.c" "C:/code/hello_world/build/CMakeFiles/main.dir/print1.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.c.obj"
+  "CMakeFiles/main.dir/print.c.obj"
+  "CMakeFiles/main.dir/print1.c.obj"
+  "libmain.dll.a"
+  "main.exe"
+  "main.exe.manifest"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
